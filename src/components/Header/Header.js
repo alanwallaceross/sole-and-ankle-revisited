20260d9b4248b2df862d9b6ca26559b973fdc1ac
alanwallaceross <35667356+alanwallaceross@users.noreply.github.com>
2024-07-02 23:48:05 +0700

    complete exercise

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import {  WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -18,18 +20,23 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
+        </LogoWrapper>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </DesktopNav>
+        <MobileActions>
+    <UnstyledButton><Icon id='shopping-bag' strokeWidth={1}></Icon></UnstyledButton>
+    <UnstyledButton><Icon id='search' strokeWidth={1}></Icon></UnstyledButton>
+    <UnstyledButton onClick={() => setShowMobileMenu(true)}><Icon id='menu' strokeWidth={1}></Icon></UnstyledButton>
+        </MobileActions>
+        <Filler />
       </MainHeader>
 
       <MobileMenu
@@ -44,29 +51,63 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+  border-bottom: 1px solid var(--color-gray-300);
+
+   @media ${QUERIES.tabletAndDown} {
+    border-top: 3px solid var(--color-gray-900);
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
-const Nav = styled.nav`
+const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
-const Side = styled.div`
+const MobileActions = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: flex;
+    gap: 32px;
+  }
+
+  @media ${QUERIES.tabletAndDown} {
+    gap: 16px;
+  }
+`
+
+const LogoWrapper = styled.div`
   flex: 1;
+
+   @media ${QUERIES.tabletAndDown} {
+    flex: revert;
+  }
+`;
+
+const Filler = styled.div`
+  flex: 1;
+
+   @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
   text-transform: uppercase;
   text-decoration: none;
-  color: ${COLORS.gray[900]};
+  color: var(--color-gray-900);
   font-weight: ${WEIGHTS.medium};
 
   &:first-of-type {
-    color: ${COLORS.secondary};
+    color: var(--color-gray-secondary);
   }
 `;
 
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,35 +1,101 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import { QUERIES, COLORS, WEIGHTS } from "../../constants";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+      <Content>
+        <UnstyledButton onClick={onDismiss}>
+          <CloseIcon id='close' />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
+        </UnstyledButton>
+        <Nav>
+        <MainLink href="/sale">Sale</MainLink>
+          <MainLink href="/new">New&nbsp;Releases</MainLink>
+          <MainLink href="/men">Men</MainLink>
+          <MainLink href="/women">Women</MainLink>
+          <MainLink href="/kids">Kids</MainLink>
+          <MainLink href="/collections">Collections</MainLink>
+
+        </Nav>
+        <Footer>
+          <FooterLink href="/terms">Terms and Conditions</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/contact">Contact Us</FooterLink>
+        </Footer>
+      </Content>
+    </Overlay>
   );
 };
+
+const CloseIcon = styled(Icon)`
+  position: absolute;
+  top: 10px;
+  right: 0px;
+  padding: 16px;
+  color: var(--color-gray-900);
+`
+
+const Overlay = styled(DialogOverlay)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: var(--color-backdrop);
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Content = styled(DialogContent)`
+  background: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 300px;
+  height: 100%;
+  padding-left: 32px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainLink = styled.a`
+  text-decoration: none;
+  color: var(--color-gray-900);
+  font-weight: ${WEIGHTS.medium};
+  font-size: ${18/16}rem;
+  text-transform: uppercase;
+
+  &:first-of-type {
+    color: var(--color-secondary)
+  }
+`;
+
+const Filler = styled.div`
+flex: 1;
+`
+
+const FooterLink = styled.a`
+text-decoration: none;
+  color: var(--color-gray-700);
+  font-weight: ${WEIGHTS.normal}
+  font-size: 1rem;
+`
 
 export default MobileMenu;
